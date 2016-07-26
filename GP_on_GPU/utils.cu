10ea2310bb1a832c28220a122b1f0fe9bcabd537
tnybny <tnybny@gmail.com>
2016-07-26 15:40:00 -0400

    dead code comment cleanup

--- GP_on_GPU/utils.cu
+++ GP_on_GPU/utils.cu
@@ -17,24 +17,3 @@ __device__ double* myrealloc(int oldsize, int newsize, double* old)
 
 __device__ double minu(double a,double b){if(a < b) return a;else return b;}
 __device__ double maxu(double a,double b){if(a > b) return a;else return b;}
-/*void ztest(double *P, double *X, int nX, double *mean, int nMean, double *sigma, int nSigma, int tail){
-  int i,j = 0,k = 0;
-  for(i = 0; i < nX; i++){
-    if(nMean > 1) j = i;
-    if(nSigma > 1) k = i;
-    switch(tail){
-    case 0:
-      P[i] = 2*gsl_cdf_ugaussian_Q(fabs((X[i] - mean[j])/sigma[k]));
-      break;
-    case 1:
-      P[i] = gsl_cdf_ugaussian_Q(fabs((X[i] - mean[j])/sigma[k]));
-      break;
-    case -1:
-      P[i] = gsl_cdf_ugaussian_P(fabs((X[i] - mean[j])/sigma[k]));
-      break;
-    default:
-      fprintf(stderr,"tail must be 0 (both) 1 (right) or -1 (left)\n");
-      return;
-    }
-  }
-}*/
